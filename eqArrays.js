7779e4b521e66f2d2cee277c35eb43433cd235ab
Shilpa Chegudi <shilpachegudi@gmail.com>
2020-08-18 20:07:51 +0000

    Adding eqArrays.js with assert function

---
+++ eqArrays.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) 
+{
+  if (actual === expected)
+    return console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
+  else
+    return console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+};
+
+function eqArrays (array1, array2)
+ {
+  if (array1.length !== array2.length)
+   {
+    return false;
+   }
+  for (let i = 0; i < array1.length; i++)
+  {
+    if (array1[i] !== array2[i])
+     {
+      return false;
+     }
+  }
+  return true;
+}
+
+//Testing Code
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2"]), false);
