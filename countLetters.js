602a5ba2686be7a10e0e8cc35ada259cb5826df4
Shilpa Chegudi <shilpachegudi@gmail.com>
2020-08-19 17:26:39 +0000

    Adding countLetters function

---
+++ countLetters.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) 
+{
+  if (actual === expected)
+    return console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
+  else
+    return console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+};
+
+function countLetters(inputString)
+{
+  let countedLetters = {};
+  let lowerCaseString = inputString.toLowerCase();
+  for (let char of lowerCaseString)
+   {
+    if (char !== " ")
+     {
+      if (countedLetters[char])
+      {
+        countedLetters[char] += 1;
+      } 
+      else
+      {
+        countedLetters[char] = 1;
+      }
+    }
+
+  }
+  console.log(countedLetters);
+  return countedLetters;
+}
+countLetters("lighthouse in the house");
+countLetters("Shilpa Chegudi");
