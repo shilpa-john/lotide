48c8cf5748521957dfc84ddbf8d2e076ab46ceb7
Shilpa Chegudi <shilpachegudi@gmail.com>
2020-08-19 01:35:36 +0000

    Adding function called without

---
+++ without.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    return console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
+  else
+    return console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+};
+
+
+//module.exports = assertEqual;
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+const eqArrays = assertEqual;
+function assertArraysEqual (array1, array2)
+ {
+  if (eqArrays(array1, array2))
+   {
+    return console.log(`👍👍👍Array Assertion Passed: ${array1} equals ${array2}`);
+  } 
+  else 
+  {
+    return console.log(`🛑🛑🛑Array Assertion Failed: ${array1} does not equal ${array2}`);
+  }
+}
+
+//without function
+function without(source, itemsToRemove) 
+{
+   let newArr = [];
+for (let i = 0; i < source.length; i ++) 
+  {
+    if (source[i] !== itemsToRemove[i]) 
+    {
+      newArr.push(source[i]);
+    }
+  }
+  return newArr;
+}
