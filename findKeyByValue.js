7a059a59936e37733fc19eab4b274ce554c95dac
Shilpa Chegudi <shilpachegudi@gmail.com>
2020-08-19 19:57:49 +0000

    Adding findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected)
+ {
+  if (actual === expected)
+    return console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
+  else
+    return console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = function(showsTable, show) 
+{
+  let showGenre;
+  let genreArray = Object.keys(showsTable);
+  for (let genre of genreArray) 
+  {
+    if (bestTVShowsByGenre[genre] === show) 
+    {
+      showGenre = genre;
+    }
+  }
+  console.log(showGenre);
+  return showGenre;
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
