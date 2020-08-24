204dd0d86206d1c79fe51195763a535b5fe8b075
Shilpa Chegudi <shilpachegudi@gmail.com>
2020-08-24 19:10:10 +0000

    Adding function takeUntil

---
+++ takeUntil.js
@@ -0,0 +1,42 @@
+//Code
+function eqArrays (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++){
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function assertArraysEqual (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    return console.log(`👍👍👍Array Assertion Passed: ${array1} equals ${array2}`);
+  } else {
+    return console.log(`🛑🛑🛑Array Assertion Failed: ${array1} does not equal ${array2}`);
+  }
+}
+
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (let element of array) {
+    if (!callback(element)){
+      results.push(element);
+    } else {
+      return results;
+    }
+  }
+}
+
+module.exports = takeUntil;
+
+
+
+
+//TESTS:
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
