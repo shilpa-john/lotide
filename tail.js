712497c01428fff1360f4f9429980fc429de367f
Shilpa Chegudi <shilpachegudi@gmail.com>
2020-08-24 18:56:40 +0000

    Cleaned up code for mocha and chai

--- tail.js
+++ tail.js
@@ -1,14 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected)
-    return console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
-  else
-    return console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-};
-const result = function(assertEqual) 
+//const assertEqual = require('./assertEqual');
+
+const tail = function(arrayInput) 
 {
-  const result = tail(["Hello", "Lighthouse", "Labs"]);
-  return assertEqual.slice(1);
-}
-  assertEqual(result.length, 2); // ensure we get back two elements
-  assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-  assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+  return arrayInput.slice(1);
+};
+
+module.exports = tail;
