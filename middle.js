2c042d6882f9ccbb2165206502aac8020b6269d3
Shilpa Chegudi <shilpachegudi@gmail.com>
2020-08-24 17:59:41 +0000

    Adding middle function after refactor

--- middle.js
+++ middle.js
@@ -1,29 +1,6 @@
-// TEST/ASSERTION FUNCTIONS
-const assertEqual = function(actual, expected)
- {
-  if (actual === expected)
-    return console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
-  else
-    return console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-};
+//const assertEqual = require('./assertEqual');
 
-const assertArraysEqual = function(actual, expected) 
-{
-
-  function assertArraysEqual (array1, array2)
-  {
-   if (eqArrays(array1, array2))
-    {
-     return console.log(`👍👍👍Array Assertion Passed: ${array1} equals ${array2}`);
-    } 
-   else 
-   {
-     return console.log(`🛑🛑🛑Array Assertion Failed: ${array1} does not equal ${array2}`);
-   }
-}
-};
-
-function middle(inputArray)
+function middlefc(inputArray)
  {
   let midArray = [];
   if (inputArray.length === 1 || inputArray.length === 2)
@@ -45,6 +22,5 @@ function middle(inputArray)
     return midArray;
   }
 };
-console.log(middle([1, 2, 3] ,[2]));
-console.log(middle([3, 4, 5], [4,5,6]));
-console.log(middle([1,2,3,4,5,6,7,8]));
+
+module.exports = middlefc;
