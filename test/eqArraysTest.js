3b3db68ab20fee870e926e8b8b39b9f81376e4cf
Shilpa Chegudi <shilpachegudi@gmail.com>
2020-08-24 13:31:26 +0000

    Adding test function after refactor

---
+++ test/eqArraysTest.js
@@ -0,0 +1,15 @@
+const assertEqual = require('../assertEqual');
+const eqArrays    = require('../eqArrays');
+
+//Testing Code
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2"]), false);
