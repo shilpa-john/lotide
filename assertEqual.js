37f1d560094c14083d2f2e65097aa8542f5dbb49
Shilpa Chegudi <shilpachegudi@gmail.com>
2020-09-15 20:59:46 +0000

    Adding file after linting and testing

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,13 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected)
-    return console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
-   else 
+    return console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  else
     return console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
 };
 
-
 module.exports = assertEqual;
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
+
+
+assertEqual("Lighthouse Labs" , "Bootcamp");
 assertEqual(1, 1);
