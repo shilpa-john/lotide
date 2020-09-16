6fd1199f8b5b9b1b0c0bcf2385508628470a5a63
Shilpa Chegudi <shilpachegudi@gmail.com>
2020-09-16 17:07:08 +0000

    Adding file after re-writing!

--- tail.js
+++ tail.js
@@ -1,8 +1,17 @@
-//const assertEqual = require('./assertEqual');
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    return console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
+  else
+    return console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+};
 
-const tail = function(arrayInput) 
+const tail = function(arrayInput)
 {
   return arrayInput.slice(1);
 };
 
-module.exports = tail;
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
