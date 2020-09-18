980615d01b889e2fe6a541aae9274082c66fb1b8
Shilpa Chegudi <shilpachegudi@gmail.com>
2020-09-18 01:19:48 +0000

    Adding countOnly function with test cases

--- countOnly.js
+++ countOnly.js
@@ -1,3 +1,9 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    return console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  else
+    return console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+};
 
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
@@ -22,26 +28,26 @@ const countOnly = function(allItems, itemsToCount)
         results[name] +=1;
       }
     };
-
-  return results;
+return results;
   };
 
-  module.exports = countOnly;
 
-/* const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];*/
-
-//const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
-
-//assertEqual(result1["Jason"], 1);
-//assertEqual(result1["Karima"], undefined);
-//assertEqual(result1["Fang"], 2);*/
+//Test Cases
+  const firstNames = [
+    "Karl",
+    "Salima",
+    "Agouhanna",
+    "Fang",
+    "Kavith",
+    "Jason",
+    "Salima",
+    "Fang",
+    "Joe"
+  ];
+  
+  const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+  
+  assertEqual(result1["Jason"], 1);
+  assertEqual(result1["Karima"], undefined);
+  assertEqual(result1["Fang"], 2);
+  assertEqual(result1["Agouhanna"], undefined);
