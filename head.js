7992bfa1f9b5efbc69dcf0ca1a709fe9328494e9
Shilpa Chegudi <shilpachegudi@gmail.com>
2020-09-22 02:47:48 +0000

    Refactored code, added mocha and chai libraries

--- head.js
+++ head.js
@@ -1,16 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected)
-    return console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
-  else
-    return console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-};
-
+const assertEqual = require('./assertEqual');
 
 const head = function(arrayInput) {
   return arrayInput[0];
 };
 
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([20, 40, 50]), 40);
+module.exports = head;
--- tail.js
+++ tail.js
@@ -1,17 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected)
-    return console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
-  else
-    return console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(arrayInput)
 {
   return arrayInput.slice(1);
 };
 
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
